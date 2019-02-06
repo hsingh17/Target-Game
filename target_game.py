2540fa31d6ccd8115870f0ac75c538ccca7b6a23
Harjot <hsingh172000@gmail.com>
2019-02-05 20:51:45 -0800

    First commit

---
+++ target_game.py
@@ -0,0 +1,156 @@
+import pygame
+import random
+import time
+import math
+
+class Target:
+    current_targets = []
+    MAX_RADIUS = 50
+    NUM_CIRCLE = 2
+
+    def __init__(self, center):
+        self.radius = 0
+        self.center = center
+        self.reach_max = False
+        if len(Target.current_targets) < Target.NUM_CIRCLE and Target.circle_inside(center) and not self.check_collision():
+            Target.current_targets.append(self)
+    
+    def check_collision(self):
+        c1_x, c1_y = self.center
+        for target in Target.current_targets:
+            c2_x, c2_y = target.center
+            distance = math.sqrt((c1_x - c2_x)**2 + (c1_y - c2_y)**2)
+            if distance <= self.radius + target.radius:
+                return True
+        return False
+
+    @classmethod
+    def vary(cls):
+        for target in Target.current_targets:
+            if target.radius != cls.MAX_RADIUS and not target.reach_max:
+                target.radius += 1 
+                if(target.radius == 50):
+                    target.reach_max = True
+            elif target.reach_max and target.radius != 0:
+                target.radius -= 1
+            else:
+                cls.current_targets.remove(target)
+                return True
+    
+    @classmethod
+    def draw(cls):
+        for target in Target.current_targets:
+            pygame.draw.circle(window, DARK_ORANGE, target.center, target.radius)
+            pygame.draw.circle(window, ORANGE, target.center, math.floor(target.radius * 0.75))
+            pygame.draw.circle(window, LIGHT_ORANGE, target.center, math.floor(target.radius * 0.50))
+            pygame.draw.circle(window, WHITE_ORANGE, target.center, math.floor(target.radius * 0.25))
+
+    @classmethod
+    def hit_check(cls, m_x, m_y):
+        for target in Target.current_targets:
+            c_x, c_y = target.center
+            distance = math.sqrt((m_x - c_x)**2 + (m_y - c_y)**2)
+            if distance <= target.radius:
+                cls.current_targets.remove(target)
+                return True
+        return False
+
+    @classmethod 
+    def circle_inside(cls, center):
+        x,y = center
+        if x + cls.MAX_RADIUS <= SCREEN_HEIGHT and x - cls.MAX_RADIUS >= 0:
+            if y + cls.MAX_RADIUS <= SCREEN_WIDTH and y - cls.MAX_RADIUS >=0:
+                return True
+        return False
+
+    @classmethod
+    def increase_difficulty(cls):
+        cls.NUM_CIRCLE += 1
+
+    @classmethod
+    def reset(cls):
+        cls.NUM_CIRCLE = 2
+        del cls.current_targets[:]
+
+
+def play_sound(path):
+    sound = SOUND_LIBRARY.get(path)
+    if sound == None:
+        sound = pygame.mixer.Sound(path)
+        SOUND_LIBRARY[path] = sound
+    sound.play()
+
+def display_score(score):
+    score = MEDIUM_FONT.render(f"{score}", True, BLACK)
+    window.blit(score, (40,70))
+
+def game_over():
+    message = LARGE_FONT.render('Game Over!', True, BLACK)
+    window.blit(message, (SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2 - 200))
+    play_again = MEDIUM_FONT.render('Press Q to Exit or R to Restart!', True, BLACK)
+    window.blit(play_again, (75, 380))
+
+def game_loop():
+    run = True
+    score = 0
+    miss = 0
+    clock = pygame.time.Clock() 
+    while run:
+        while miss == 3:
+            game_over()
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        miss = 0 
+                        run = False
+                    elif event.key == pygame.K_r:
+                        Target.reset()
+                        game_loop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                if Target.hit_check(x,y):
+                    score += 1
+                    if score != 0 and score % 10 == 0:
+                        Target.increase_difficulty()
+                    play_sound('pop.wav')
+                else:
+                    play_sound('bullet.wav')
+        
+        new_target = Target((random.randint(1, SCREEN_HEIGHT), random.randint(1, SCREEN_WIDTH)))
+        if Target.vary():
+            play_sound('miss.wav')
+            miss += 1
+
+        window.fill(WHITE)
+        Target.draw()
+        display_score(score)
+        clock.tick(30)
+        pygame.display.update()
+
+    pygame.quit
+
+
+pygame.font.init()
+pygame.init()
+
+WHITE_ORANGE = (255,225,164)
+DARK_ORANGE = (255,189,55)
+LIGHT_ORANGE = (255,217,139)
+ORANGE = (255,203,97)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 700
+LARGE_FONT = pygame.font.SysFont("Bit5x3 Regular", 100)
+MEDIUM_FONT = pygame.font.SysFont("Bit5x3 Regular", 50)
+SOUND_LIBRARY = {}
+
+window = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
+pygame.display.set_caption("Target Game")
+
+game_loop()
